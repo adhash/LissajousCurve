6210568e6237051b7729ac0673c8c85901157f93
adhash <wojciech.piecha@gmail.com>
2021-02-15 12:17:47 +0100

    Update Pointer.swift

--- LissajousCurve/UI/Graphics/Pointer.swift
+++ LissajousCurve/UI/Graphics/Pointer.swift
@@ -1,11 +1,3 @@
-//
-//  Pointer.swift
-//  LissajousCurve
-//
-//  Created by wojtek on 22/09/2020.
-//  Copyright © 2020 wojtek. All rights reserved.
-//
-
 import UIKit
 
 class Pointer: UIView {
